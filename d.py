ac0f503fe8f78071a1736407c4c051d2bfadda75
diana2101-041 <72841630+diana2101-041@users.noreply.github.com>
2020-10-16 23:59:25 -0500

    Add files via upload

---
+++ d.py
@@ -0,0 +1,13 @@
+def create_phone_number(n):
+	n = str(input ("inserte 10 numeros de 1 al 9"))
+	l = (1,2,3,4,5,6,7,8,9,)
+	for i in l :
+		print (i)
+	return (n)
+
+	print(n)
+#your code here
+    
+   
+  
+create_phone_number("diana")
